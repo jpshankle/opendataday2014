aa0a7928e60bbb12ecf16818d1e33223219da482
Jeremy Shankle <jpshankle@gmail.com>
2014-02-22 14:38:39 -0500

    add bower flask mongoengine

--- home.py
+++
@@ -1,7 +0,0 @@
-import cherrypy
-class CorruptionData(object):
-    def index(self):
-        return "Corruption Data"
-    index.exposed = True
-
-cherrypy.quickstart(CorruptionData())
---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine, MongoEngineSessionInterface
+import pymongo
+
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+app.config["MONGODB_SETTINGS"] = {
+    'DB': 'stop_corruption'
+}
+
+db = MongoEngine(app)
+app.session_interface = MongoEngineSessionInterface(db)
+
+class Source(db.Document):
+    country = db.StringField(required=True)
+    region = db.StringField(required=True)
+    language = db.StringField(required=True)
+    currency = db.StringField(required=True)
+
+class Contract(db.Document):
+    source = db.StringField(required=True)
+    project_name = db.StringField(required=True)
+    description = db.StringField()
+    procument_method = db.StringField()
+    procument_category = db.StringField()
+    organization = db.StringField()
+    sector = db.StringField()
+    supplier = db.StringField()
+    supplier_country = db.StringField()
+    fiscal_year = db.StringField()
+    contract_signed = db.StringField()
+    tender_issued = db.StringField()
+    region = db.StringField()
+    date = db.DateTimeField()
+    amount = db.IntField()
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == "__main__":
+    app.run()
